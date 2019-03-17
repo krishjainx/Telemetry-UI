386b48d6e8cf83d07aa8a0a349a79dec8f22b0e5
Chris Dalke <chrisdalke@gmail.com>
2019-03-17 13:24:17 -0400

    Removed references to server release bundles

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ var Chart = require('chart.js');
 var child_process = require('child_process');
 var kill = require('tree-kill');
 
-/*
-var serverProcess = child_process.execFile('server-bundles/release-1.0.1');
-serverProcess.stdout.on('data', function(data) {
-    console.log('[Telemetry Server] ' + data);
-});
-serverProcess.stderr.on('data', function(data) {
-    console.log('[Telemetry Server]  ' + data);
-});
-serverProcess.on('close', function(code) {
-    console.log('[Telemetry Server] Exiting server with status: ' + code);
-});
-*/
-
 let win
 
 function createWindow () {
@@ -38,6 +25,5 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => { app.quit(); })
 app.on('activate', () => { if (win === null) { createWindow(); }})
 app.on('before-quit', function(){
-    //serverProcess.kill();
     console.log("Quitting...");
 });
